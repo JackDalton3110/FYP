c2f7ac00ec953f20f480ac00267858c300c792e6
JackDalton3110 <c00205943@itcarlow.ie>
2019-03-27 20:50:08 +0000

    Update ANN

--- Flocking/flockingNN.py
+++ Flocking/flockingNN.py
@@ -1,4 +1,4 @@
-from flockAlgorithm import Bird
+from flockAlgorithm import main, Bird
 
 import tensorflow as tf
 import keras
@@ -13,7 +13,7 @@ import pickle
 
 
 class flockNN:
-    def __init__ (self, initial_games = 10000, test_games = 1000, goal_steps = 2000, lr = 1e-2, filename = 'flock_nn.tflearn'):
+    def __init__ (self, initial_games = 10000, test_games = 1000, goal_steps = 2000, lr = 1e-2, filename = 'testData.txt'):
         self.initial_games = initial_games
         self.test_games = test_games
         self.goal_steps = goal_steps
@@ -26,30 +26,16 @@ class flockNN:
             [[0, -1], 3]
             ]
 
-        def generate_action(self, Bird):
-            action = randint(0,2)-1
-            return action, self.get_game_action(Bird, action)
-
-        def get_game_action(self, Bird, action):
-            if action == -1:
-                new_direction = self.turn_vector_to_the_left(Bird_direction)
-            elif action == 1:
-                new_direction = self.turn_vector_to_the_right(Bird_direction)
-            for pair in self.vectors_and_keys:
-                if pair[0] == new_direction.tolist():
-                    game_action = pair[1]
-            return game_action
 
     def model(self):
-        network = input_data(shape=[None,12,1], name='input')
-        network = fully_connected(network,144,ativation='relu')
-        network = fully_connected(network, 2,ativation='linear')
+        network = input_data(shape=[None, 5, 1], name='input')
+        network = fully_connected(network, 6, activation='relu')
+        network = fully_connected(network, 1, activation='linear')
         model = tflearn.DNN(network, tensorboard_dir='log')
         return model
 
     def visualise_game(self, model):
-        game = flockAlgorithm()
-        prev_observation = self.generate_observation(distance, position)
+        game = main()
         for i in range(self.goal_steps):
             predictions = []
             for action in range(-1, 2):
@@ -79,13 +65,13 @@ class flockNN:
             i, 
 
     def train(self):
-        training_data = self.initial_population()
+        training_data = self.filename
         nn_model = self.model()
         self.test_model(nn_model)
 
     def visualise(self):
         nn_model = self.model()
-        nn_mdoel.load(self.filename)
+        nn_model.load(self.filename)
         self.visualise_game(nn_model)
 
     def test(self):
@@ -93,9 +79,6 @@ class flockNN:
         nn_model.load(self.filename)
         self.test_model(nn_model)
 
-    if __name__ == "main":
-        flockNN().train()
-        flockNN().visualise()
-
-
-    
+if __name__ == "__main__":
+    flockNN().train()
+    #flockNN().visualise()
